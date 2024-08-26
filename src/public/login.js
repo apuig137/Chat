9daa1b973b2e4c1957337048d9e36f95804e8e2c
Agustin Puig <apuig137@gmail.com>
2024-08-25 23:56:52 -0300

    cors III

--- src/public/login.js
+++ src/public/login.js
@@ -5,7 +5,7 @@ form.addEventListener('submit', e => {
     const data = new FormData(form);
     const obj = {};
     data.forEach((value, key) => obj[key] = value);
-    fetch('/user/login', {
+    fetch('https://livechat-zk2w.onrender.com/user/login', {
         method: 'POST',
         body: JSON.stringify(obj),
         headers: {
